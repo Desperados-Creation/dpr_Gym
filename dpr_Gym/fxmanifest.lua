0ad978544d90ddea8fce8dd5a75cdfcbeeba389f
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-04 14:30:32 +0200

    Update fxmanifest.lua

--- dpr_Gym/fxmanifest.lua
+++ dpr_Gym/fxmanifest.lua
@@ -12,7 +12,8 @@ client_scripts {
     "src/menu/panels/*.lua",
     "src/menu/windows/*.lua",
 
-    "client/*.lua",
+    "client/cl_superettelua",
+    "client/client_exercice"
 }
 
 server_scripts {
